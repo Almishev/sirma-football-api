7ec328fbb1ca06dd67bee88ebccd5b4186f6f853
Anton Almishev <antonalmishev@abv.bg>
2026-03-17 19:42:28 +0200

    add stripe config class

---
+++ src/main/java/com/sirma/football_api/config/StripeConfig.java
@@ -0,0 +1,22 @@
+package com.sirma.football_api.config;
+
+import com.stripe.Stripe;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StripeConfig {
+
+    private final String secretKey;
+
+    public StripeConfig(@Value("${stripe.secret-key}") String secretKey) {
+        this.secretKey = secretKey;
+    }
+
+    @PostConstruct
+    public void init() {
+        Stripe.apiKey = secretKey;
+    }
+}
+
